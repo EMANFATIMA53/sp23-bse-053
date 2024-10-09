cbf1221e3a3941e6eaf7b76bdafb309026ee73ce
eman fatima <sp23-bse-053@cuilahore.edu.pk>
2024-10-10 00:19:30 +0500

    file name changed

--- first.js
+++ first.js
@@ -1,3 +1,12 @@
 console.log("hell o hello js");
-console.log("phass gai tum");
-console.log("ther is no back home");
+var x=5;
+var y=9;
+console.log(x * y);
+
+function sum(a,b){
+    return a+b;
+}
+console.log(sum(x,y));
+
+var mysum=sum;
+console.log(mysum);
